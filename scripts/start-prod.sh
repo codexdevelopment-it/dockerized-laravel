9481819c95d5ca6c56ffc35aaf8ad3c483c1ef9b
Murkrow <marcocoppola02.mc@gmail.com>
2023-11-18 00:02:05 +0100

    Fixed prod script

--- scripts/start-prod.sh
+++ scripts/start-prod.sh
@@ -5,6 +5,26 @@ export $(grep -v '^#' .env | xargs)
 docker compose down
 docker compose -f docker-compose.yml -f docker-compose-prod.yml  up -d
 
+# Ensure framework folders exist
+docker exec "${CONTAINER_NAME}" mkdir -p /var/www/storage/framework/sessions
+docker exec "${CONTAINER_NAME}" mkdir -p /var/www/storage/framework/views
+docker exec "${CONTAINER_NAME}" mkdir -p /var/www/storage/framework/cache
+
+# Ensure project directory is readable
+docker exec "${CONTAINER_NAME}" chmod 775 /var/www
+
+# Ensure public folder is readable
+docker exec "${CONTAINER_NAME}" chmod -R 775 public
+
+# Ensure storage public folder is readable and future created files will be readable
+docker exec "${CONTAINER_NAME}" chmod +x /var/www
+docker exec "${CONTAINER_NAME}" chmod +x /var/www/storage
+docker exec "${CONTAINER_NAME}" mkdir -p /var/www/storage/app
+docker exec "${CONTAINER_NAME}" chmod +x /var/www/storage/app
+docker exec "${CONTAINER_NAME}" mkdir -p /var/www/storage/app/public
+docker exec "${CONTAINER_NAME}" chmod +x /var/www/storage/app/public
+docker exec "${CONTAINER_NAME}" bash -c "setfacl -Rm d:u::rwx,d:g::rwx,d:o::rX /var/www/storage/app/public/"
+
 # Ensure working directory is owned by sail (if user sail exists)
 # THIS IS BETTER TO DO BEFORE STARTING THE SCRIPT (BY HAND)
 # in this way the script can be run by non-root users
@@ -23,7 +43,6 @@ docker exec  "${CONTAINER_NAME}" php artisan storage:link
 # The X for directories is very useful to ensure that new files and directories created there will be accessible
 docker exec  "${CONTAINER_NAME}" setfacl -R -d -m u::rwX,g::rwX,o::rX /var/www/storage/app/public/
 
-
 # Check that APP_KEY is set
 if [ -z "$APP_KEY" ]; then
     echo "APP_KEY is not set, generating one"
@@ -37,7 +56,3 @@ docker exec "${CONTAINER_NAME}" npm install
 # Compile assets
 echo "Compiling assets"
 docker exec  "${CONTAINER_NAME}" npm run build
-
-
-# Run the queue worker (optional)
-#docker exec  "${CONTAINER_NAME}" php artisan queue:work --timeout=0
