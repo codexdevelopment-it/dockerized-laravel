469dace2c1f0b25af1d6539e8fe7f1f0b1c47d99
Murkrow <marcocoppola02.mc@gmail.com>
2024-06-21 12:12:48 +0200

    Fixed application path

--- scripts/start.sh
+++ scripts/start.sh
@@ -53,36 +53,43 @@ echo -e "${BLUE}${COMPOSE_COMMAND}${NC}"
 eval "${DOWN_COMMAND}"
 eval "${UP_COMMAND}"
 
+DOCKER_APP_DIR=/var/www/${CONTAINER_NAME}
+
 # Ensure framework folders exist
-docker exec "${CONTAINER_NAME}" mkdir -p /var/www/storage/framework/sessions
-docker exec "${CONTAINER_NAME}" mkdir -p /var/www/storage/framework/views
-docker exec "${CONTAINER_NAME}" mkdir -p /var/www/storage/framework/cache
+docker exec "${CONTAINER_NAME}" mkdir -p "${DOCKER_APP_DIR}"/storage/framework/sessions
+docker exec "${CONTAINER_NAME}" mkdir -p "${DOCKER_APP_DIR}"/storage/framework/views
+docker exec "${CONTAINER_NAME}" mkdir -p "${DOCKER_APP_DIR}"/storage/framework/cache
 
 # Ensure project directory is readable
-docker exec "${CONTAINER_NAME}" chmod 775 /var/www
+docker exec "${CONTAINER_NAME}" chmod 775 "${DOCKER_APP_DIR}"
 
 # Ensure public folder is readable
-docker exec "${CONTAINER_NAME}" chmod -R 775 public
-docker exec "${CONTAINER_NAME}" bash -c "setfacl -R -m d:u::rwx,d:g::rwx,d:o::rX /var/www/public"
+docker exec "${CONTAINER_NAME}" chmod -R 775 "${DOCKER_APP_DIR}"/public
+docker exec "${CONTAINER_NAME}" bash -c "setfacl -R -m d:u::rwx,d:g::rwx,d:o::rX ${DOCKER_APP_DIR}/public"
 
 # Link storage folder
 docker exec  "${CONTAINER_NAME}" php artisan storage:link
 
 # Ensure storage public folder is readable and future created files will be readable
-docker exec "${CONTAINER_NAME}" chmod +x /var/www
-docker exec "${CONTAINER_NAME}" chmod +x /var/www/storage
-docker exec "${CONTAINER_NAME}" mkdir -p /var/www/storage/app
-docker exec "${CONTAINER_NAME}" chmod +x /var/www/storage/app
-docker exec "${CONTAINER_NAME}" mkdir -p /var/www/storage/app/public
-docker exec "${CONTAINER_NAME}" chmod +x /var/www/storage/app/public
+docker exec "${CONTAINER_NAME}" chmod +x "${DOCKER_APP_DIR}"
+docker exec "${CONTAINER_NAME}" chmod +x "${DOCKER_APP_DIR}"/storage
+docker exec "${CONTAINER_NAME}" mkdir -p "${DOCKER_APP_DIR}"/storage/app
+docker exec "${CONTAINER_NAME}" chmod +x "${DOCKER_APP_DIR}"/storage/app
+docker exec "${CONTAINER_NAME}" mkdir -p "${DOCKER_APP_DIR}"/storage/app/public
+docker exec "${CONTAINER_NAME}" chmod +x "${DOCKER_APP_DIR}"/storage/app/public
 
 # Set default permissions for storage public folder (always read for everyone) with ACL
 # Not that capital X means execute only if it is a directory or already has execute permission for some user
 # The X for directories is very useful to ensure that new files and directories created there will be accessible
-docker exec "${CONTAINER_NAME}" setfacl -R -d -m u::rwX,g::rwX,o::rX /var/www/storage/app/public/
+docker exec "${CONTAINER_NAME}" setfacl -R -d -m u::rwX,g::rwX,o::rX "${DOCKER_APP_DIR}"/storage/app/public/
 
 # Install composer packages (read container name from environment variable)
-docker exec "${CONTAINER_NAME}" composer install --optimize-autoloader --no-dev
+COMPOSER_COMMAND="docker exec ${CONTAINER_NAME} composer install"
+if [ "$APP_ENV" == "production" ]; then
+    COMPOSER_COMMAND="${COMPOSER_COMMAND} --optimize-autoloader --no-dev"
+fi
+echo "Installing composer packages"
+eval "${COMPOSER_COMMAND}"
 
 # Check that APP_KEY is set
 if [ -z "$APP_KEY" ]; then
@@ -111,15 +118,16 @@ fi
 
 # Start the SERVER
 if [ "$SERVER" == "octane" ]; then
-    docker exec -d "${CONTAINER_NAME}" php -d variables_order=EGPCS \
-                                            /var/www/artisan octane:start \
-                                            --SERVER=frankenphp \
-                                            --host=0.0.0.0 \
-                                            --admin-port=2019 \
-                                            --port=80
+  echo "FIX OCTANE SERVER"
+#    docker exec -d "${CONTAINER_NAME}" php -d variables_order=EGPCS \
+#                                            /var/www/artisan octane:start \
+#                                            --SERVER=frankenphp \
+#                                            --host=0.0.0.0 \
+#                                            --admin-port=2019 \
+#                                            --port=80
 fi
 
 if [ "$SERVER" == "artisan" ]; then
 docker exec -it "${CONTAINER_NAME}" php -d variables_order=EGPCS \
-                                        /var/www/artisan serve --host=0.0.0.0 --port=80
+                                        "${DOCKER_APP_DIR}"/artisan serve --host=0.0.0.0 --port=80
 fi
