61339936b48e56c52fd7406c24c5540b5b17c984
murkrow <marcocoppola02.mc@gmail.com>
2025-02-03 00:19:52 +0100

    Create app

--- configure-app.sh
+++ configure-app.sh
@@ -1,78 +1,56 @@
-# Ask if need to create a new app or add to existing app
-echo "Do you want to create a new app or add to an existing app? (new/existing)"
-read -r APP_TYPE
+#!/bin/bash
 
-# Remove previous dockerized-laravel directory if exists
-if [ -d "dockerized-laravel" ]; then
-    rm -rf dockerized-laravel
-fi
+set -e  # Exit immediately if a command exits with a non-zero status.
 
-# Ask user for app name
-echo "Enter the name of the app"
-read -r APP_NAME
+# Create configuration for a new or existing Laravel app
+read -r -p "Do you want to create a new app or add to an existing app? (new/existing) " APP_TYPE
+read -r -p "Enter the name of the app: " APP_NAME
+read -r -p "Enter the container base name (e.g., 'laravel' -> 'laravel-mariadb', 'laravel-nginx', etc.): " CONTAINER_BASE_NAME
+read -r -p "Enter the repository URL (optional): " REPO_URL
+read -r -p "Enter the database name (default: $CONTAINER_BASE_NAME): " DB_NAME
+DB_NAME=${DB_NAME:-$CONTAINER_BASE_NAME}  # Use default if empty
 
-# Ask user for container base name
-echo "Enter the name of the container, all containers will be prefixed with this name (e.g. 'laravel' will result in 'laravel-mariadb', 'laravel-nginx', etc.)"
-read -r CONTAINER_BASE_NAME
+# Cleanup previous setup if exists
+[ -d "dockerized-laravel" ] && rm -rf dockerized-laravel
 
-# Ask user for repo url of the project
-echo "Enter the url of the repository, this will be used to deploy the project (you can skip this for now)"
-read -r REPO_URL
-
-# Ask user for database name (default to CONTAINER_BASE_NAME)
-echo "Enter the name of the database (default is $CONTAINER_BASE_NAME)"
-read -r DB_NAME
-if [ -z "$DB_NAME" ]; then
-    DB_NAME=$CONTAINER_BASE_NAME
-fi
-
-# Clone this repo inside project root directory
 git clone https://github.com/Murkrow02/dockerized-laravel
-cd dockerized-laravel || exit
+cd dockerized-laravel || exit 1
 
-# Create .env file
-# Sed works differently on Mac and Linux
-if [[ $(uname) == "Darwin" ]]; then
-    sed -i '' "s/{{APP_NAME}}/$APP_NAME/g" .env
-    sed -i '' "s/{{CONTAINER_NAME}}/$CONTAINER_BASE_NAME/g" .env
-    sed -i '' "s/{{CONTAINER_NAME}}/$CONTAINER_BASE_NAME/g" docker/fpm.conf
-    sed -i '' "s/{{CONTAINER_NAME}}/$CONTAINER_BASE_NAME/g" docker/nginx.conf
-    sed -i '' "s/{{DB_NAME}}/$DB_NAME/g" .env
-    sed -i '' "s|{{REPO_URL}}|$REPO_URL|g" .env
-else
-    sed -i "s/{{APP_NAME}}/$APP_NAME/g" .env
-    sed -i "s/{{CONTAINER_NAME}}/$CONTAINER_BASE_NAME/g" .env
-    sed -i "s/{{CONTAINER_NAME}}/$CONTAINER_BASE_NAME/g" docker/fpm.conf
-    sed -i "s/{{CONTAINER_NAME}}/$CONTAINER_BASE_NAME/g" docker/nginx.conf
-    sed -i "s/{{DB_NAME}}/$DB_NAME/g" .env
-    sed -i "s|{{REPO_URL}}|$REPO_URL|g" .env
-fi
+chmod +x scripts/*
 
-# If we are creating a new app, start the docker containers and install laravel
+# Update .env and configuration files
+update_config() {
+    local file=$1
+    local mac_sed_flag=""
+    [[ $(uname) == "Darwin" ]] && mac_sed_flag="''"
+
+    sed -i $mac_sed_flag "s/{{APP_NAME}}/$APP_NAME/g" "$file"
+    sed -i $mac_sed_flag "s/{{CONTAINER_NAME}}/$CONTAINER_BASE_NAME/g" "$file"
+    sed -i $mac_sed_flag "s/{{DB_NAME}}/$DB_NAME/g" "$file"
+    sed -i $mac_sed_flag "s|{{REPO_URL}}|$REPO_URL|g" "$file"
+}
+
+# Update configuration files by replacing placeholders
+for file in .env docker/fpm.conf docker/nginx.conf; do
+    [ -f "$file" ] && update_config "$file"
+done
+
 if [ "$APP_TYPE" == "new" ]; then
-
-    # Start the containers
     ./scripts/containers-up.sh
+    docker exec "$CONTAINER_BASE_NAME" composer global require laravel/installer
+    docker exec -it "$CONTAINER_BASE_NAME" sh -c "~/.composer/vendor/bin/laravel new $CONTAINER_BASE_NAME"
 
-    # Install laravel
-    docker exec "${CONTAINER_BASE_NAME}" composer global require laravel/installer
-    docker exec "${CONTAINER_BASE_NAME}" "~/.composer/vendor/laravel/installer/bin/laravel new"
+    for dir in docker scripts; do
+        cp -r "$dir" "$CONTAINER_BASE_NAME"
+    done
+
+    mv .env "$CONTAINER_BASE_NAME/.env"
+else
+    mv ../.env ../.env.old || true
+    cp .env ../.env
+    cp -r docker ../docker
+    cp -r scripts ../scripts
+    cd ..
 fi
 
-
-# Copy the .env file to the root directory of the project
-mv ../.env ../.env.old || true
-cp .env ../.env
-
-# Copy docker folder
-cp -r docker ../docker
-
-# Copy the scripts to the root directory of the project
-cp -r scripts ../scripts
-
-# Install packages
-cd ..
-#TODO
-
-# Remove the dockerized-laravel directory
 rm -rf dockerized-laravel
